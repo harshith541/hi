820e74ae61772bd112d8121908a22647f1912c0b
Harshith Shetty <your_github_email@example.com>
2026-06-26 12:56:01 +0530

    ffhf

--- hell.c
+++ hell.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    printf("Enter two numbers:");
+     int a,b;
+     printf("Enter two numbers:");
     scanf("%d%d",&a,&b);
     if(a>b)
         printf("%d is greater than %d",a,b);
-     else if(b>a)
+      else if(b>a)
          printf("%d is greater than %d",b,a);
      else
         printf("Both numbers are equal");
