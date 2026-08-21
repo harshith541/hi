dddecc6d68c94ed4dc7536c73b002e5c9336f50b
Harshith.A <harshithshetty803@gmail.com>
2026-08-21 22:03:42 +0530

    mkygvcff

---
+++ ppp.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+tips = sns.load_dataset("tips")
+sns.regplot(x="total_bill", y="tip", data=tips)
+plt.title("Bill vs Tip Relationship")
+plt.show()
