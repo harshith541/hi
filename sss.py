ec0882e928a87d3a1fb32e48ac1cd30ae441e5af
Harshith.A <harshithshetty803@gmail.com>
2026-08-28 20:01:20 +0530

    vggg

--- sss.py
+++ sss.py
@@ -5,6 +5,6 @@ plt.plot(x,y,marker='o',color='teal', linewidth=2)
 plt.title("SalesGrowthOver5Years")
 plt.xlabel("Year")
 plt.ylabel("Sales(inlakhs)")
-plt.legend(["Sales"], loc='upperleft')
+plt.legend(["Sales"],   loc='upperleft')
 plt.annotate("HighestGrowth",xy=(5,10),xytext=(3.5,9),
 arrowprops=dict(facecolor='black',arrowstyle='->'))
