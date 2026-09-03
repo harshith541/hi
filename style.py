17a031f372c292eb73ebfba37d436c567f861f2e
Harshith.A <harshithshetty803@gmail.com>
2026-09-03 21:51:12 +0530

    cvff

--- style.py
+++ style.py
@@ -5,7 +5,7 @@ plt.style.use('seaborn-v0_8-darkgrid')
 #ConfiguringglobalsettingsviarcParams
 plt.rcParams['figure.figsize']=[8,5]
 plt.rcParams['axes.titlesize']=16
-plt.rcParams['axes.labelsize']=14
+plt.rcParams ['axes.labelsize']=14
 plt.rcParams ['lines.linewidth']=2.5
 x=np.linspace(0,10,100)
 y1=np.sin(x)
