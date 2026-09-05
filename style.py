7a712beacb82bfcb3dae677cc7211fa384059eea
Harshith.A <harshithshetty803@gmail.com>
2026-09-05 21:55:43 +0530

    cdsww

--- style.py
+++ style.py
@@ -3,7 +3,7 @@ importnumpyasnp
 #Applyingastylesheet
 plt.style.use('seaborn-v0_8-darkgrid')
 #ConfiguringglobalsettingsviarcParams
-plt.rcParams['figure.figsize']=[8,5]
+plt.rcParams ['figure.figsize']=[8,5]
 plt.rcParams ['axes.titlesize']=16
 plt.rcParams ['axes.labelsize']=14
 plt.rcParams ['lines.linewidth']=2.5
