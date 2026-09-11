cb18db9f1d17136b36fecda8527c33970c504f9c
Harshith.A <harshithshetty803@gmail.com>
2026-09-11 21:17:17 +0530

    njuiu

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 x =np.linspace  (0, 10, 100)
 y =np.sin(x)
-plt.style.use('dark_background')
+plt.style.use ('dark_background')
 plt.plot(x, y)
 plt.title   ("Sine Wave with dark_background Style")
 plt.show()
